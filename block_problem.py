9a1c05a82762e37b4d62991703c1c431c15bf5d5
jameswong95 <39819869+jameswong95@users.noreply.github.com>
2019-02-22 11:16:51 +0800

    Add files via upload

---
+++ block_problem.py
@@ -0,0 +1,19 @@
+from functions import *
+
+
+initial_state = ["a", "e", "c", "b", "d"]
+table = []
+end_state = []
+
+
+Armempty()
+unstack(initial_state[0],initial_state[1])
+holding(initial_state[0])
+putdown(initial_state[0])
+ontable(initial_state[0])
+clear(initial_state[0])
+clear(initial_state[1])
+#
+#
+# for i in range(len(initial_state)):
+#     print
---
+++ functions.py
@@ -0,0 +1,31 @@
+def unstack(a,b):
+    print "Pick up clear block " + a + " from "+ b
+    pass
+def stack(a,b):
+    print "Place "+a +" using the arm onto clear block "+ b
+    pass
+def pickup(a):
+    print "Lift clear block "+a+ " with the empty arm"
+    pass
+def putdown(a):
+    print "Place the held block "+ a + " onto a free space on the table"
+    pass
+
+
+
+
+def on(a,b):
+    print "Block " +a+" is on Block "+ b
+    pass
+def ontable(a):
+    print "Block "+a+ " is on the table"
+    pass
+def clear(a):
+    print "Block "+a+ " is clear"
+    pass
+def holding(a):
+    print "Arm is holding "+a
+    pass
+def Armempty():
+    print "Arm is Empty"
+    pass
