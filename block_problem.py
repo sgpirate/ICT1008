c693154b90e7c7da8949c0bfb2bd6b851d626483
jameswong <jameswongsk@gmail.com>
2019-02-26 00:25:47 +0800

    ABC

--- block_problem.py
+++ block_problem.py
@@ -60,6 +60,7 @@ if "on_" in goal_state[-1]:
     Armempty()
     action_unstack(last_ele[1],table,last_ele[0])
     goal_state.append("move_"+last_ele[1]+last_ele[0])
+    goal_state.append("on_"+last_ele[1]+"_Table"+" clear_"+last_ele[1]+" clear_" +last_ele[0])
 
 
 # print new_list[1]
