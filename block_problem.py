afe3def01f0557c9e08b54be9e305b51c715c423
jameswong <jameswongsk@gmail.com>
2019-02-25 22:36:08 +0800

    ABC

--- block_problem.py
+++ block_problem.py
@@ -3,22 +3,27 @@ from block_stack import *
 
 
 
-start_state = "B AC"  #Start State
+start_state = "B AC EFG"  #Start State
 final_state = "CBA" #Goal State
 initial_state_ = []
 goal_state = []
 current_state = []
 initial_state_ = start_state.split()
+goal_state = final_state.split()
+print "Initial State:"
 for i in range(len(initial_state_)):
     if len(initial_state_[i]) == 1:
+        ontable(initial_state_[i], table)
         print clear(initial_state_[i])
-        ontable(initial_state_[i],table)
+        print
     else:
         for j in range(len(initial_state_[i])):
+            if j == 0:
+                ontable(initial_state_[i][j], table)
             if j == len(initial_state_[i])-1:
                 print clear(initial_state_[i][j])
+                print
             else:
                 print on(initial_state_[i][j+1],initial_state_[i][j])
-                ontable(initial_state_[i][j],table)
 
-
+# print goal_state
